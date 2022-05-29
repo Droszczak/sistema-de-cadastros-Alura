ccfeecb39fd56e68a3f2c725b534facfe883c621
Júlio César Droszczak <68972922+Droszczak@users.noreply.github.com>
2022-05-29 20:58:33 -0300

    mais um coonsole log de teste:

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um coonsole log de teste:
+console.log(""O Paulo fez este commit sem você ver na aula!")
