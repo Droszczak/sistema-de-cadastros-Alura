f44a146c0b3e56faa5e7c7edd69b5c6a3ce1753a
droszczak <julio.droszczak@ufpr.br>
2022-05-29 21:03:50 -0300

    Enviando para o GitHub

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um coonsole log de teste:
-console.log(""O Paulo fez este commit sem você ver na aula!")
+console.log("O Paulo fez este commit sem você ver na aula!")
+console.log("Enviando para oo GitHub")
