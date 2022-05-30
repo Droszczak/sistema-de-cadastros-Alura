d4d15a674ca64da2a3516bd2e9a4e6386608301b
droszczak <julio.droszczak@ufpr.br>
2022-05-29 21:12:49 -0300

    Voltando arquivo a esta anterior

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um coonsole log de teste:
-console.log("O Paulo fez este commit sem você ver na aula!")
-console.log("Enviando para oo GitHub")
+console.log(""O Paulo fez este commit sem você ver na aula!")
